908b5b503dbe7539cd7d9bb2c367e194166b1360
Arthur Azoula <arthur.azoula@epitech.eu>
2023-09-28 15:18:06 +0200

    Add About

--- Portfolio/src/components/About.jsx
+++ Portfolio/src/components/About.jsx
@@ -1,17 +1,29 @@
 import React from "react";
+import CodingSvg from "../icons/Code"; // Remplacez par le chemin de votre fichier SVG de codage
+import WorldSvg from "../icons/World"; // Remplacez par le chemin de votre fichier SVG de monde
 
 const About = () => {
     return (
-        <div>
-            <h1>À propos de moi</h1>
-            <p>Salut à tous ! Je suis Arthur, un jeune étudiant en informatique passionné par la technologie.</p>
-            <p>J'ai toujours été fasciné par le monde de l'informatique, et ma véritable passion se révèle (surtout dans les jeux vidéo) !</p>
-            <p>Mon parcours m'a conduit à étudier l'informatique en DUT à l'IUT de Reims de 2020 à 2022, où j'ai découvert mon amour pour la programmation. C'était un peu comme tomber amoureux, mais avec moins de fleurs et plus de caféine. Après deux ans d'aventures, de projets et même d'un stage, j'ai obtenu mon DUT !</p>
-            <p>Mais il était encore trop tôt pour m'arrêter en si bon chemin. J'ai poursuivi mes études à l'Université de Lille en 3ème année de Licence Informatique, où j'ai appris énormément de choses et été confronté à de nouvelles technologies passionnantes. J'ai relevé le défi avec détermination.</p>
-            <p>De plus, j'ai eu la chance de réaliser un deuxième stage en entreprise, ce qui a renforcé mon désir de développer mes compétences informatiques dans le monde professionnel.</p>
-            <p>Actuellement, je suis enthousiaste à l'idée de démarrer une nouvelle aventure à Epitech pour ces trois prochaines années en alternance, poursuivant ainsi mon voyage dans le monde de l'informatique.</p>
-            <p>En dehors de l'informatique, j'aime passer du temps avec mes amis, me plonger dans des jeux vidéo et explorer le monde. Mais ne vous méprenez pas, je suis aussi sérieux dans mes projets professionnels que je le suis dans mes parties de jeux vidéo. Je suis déterminé à devenir un expert dans mon domaine et à faire une différence dans le monde.</p>
-            <p>Si vous souhaitez discuter de code ou de tout autre sujet, n'hésitez pas à me contacter sur LinkedIn ou par e-mail à <a href="mailto:arthur2504azoula@gmail.com">arthur2504azoula@gmail.com</a>.</p>
+        <div className="bg-chamoisee text-champagne p-6 m-12 rounded-lg">
+            <div className="container mx-auto">
+                <h1 className="text-4xl font-bold mb-4">À propos de moi</h1>
+                <div className="flex flex-col lg:flex-row mb-8">
+                    <div className="lg:w-1/2 mr-6">
+                        <p className="text-lg mb-4">
+                            Salut à tous ! Je suis Arthur, un jeune étudiant en informatique passionné par la technologie.
+                        </p>
+                        <p className="text-lg">
+                            J'ai toujours été fasciné par le monde de l'informatique, et ma véritable passion se révèle (surtout dans les jeux vidéo) !
+                        </p>
+                    </div>
+                    <div className="lg:w-1/2 mt-6 lg:mt-0">
+                        {/* SVG d'un personnage qui code */}
+                        <div className="w-32 h-32 mx-auto lg:mx-0">
+                            <CodingSvg />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- Portfolio/src/components/Header.jsx
+++ Portfolio/src/components/Header.jsx
@@ -1,46 +1,94 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import profileImage from "../assets/images/me.png";
+import School from "../icons/School";
+import User from "../icons/User";
+import Experience from "../icons/Experience";
+import Contact from "../icons/Contact";
+import Skills from "../icons/Skills"
 
 const Header = () => {
     const [isNavOpen, setNavOpen] = useState(false);
+    const [showTitle, setShowTitle] = useState(false);
+    const [animatedTitle, setAnimatedTitle] = useState("");
+    const titleText = "Arthur's Portolio...";
 
     const toggleNav = () => {
         setNavOpen(!isNavOpen);
     };
 
+    useEffect(() => {
+        const titleTimeout = setTimeout(() => {
+            setShowTitle(true);
+        }, 10000);
+
+        return () => clearTimeout(titleTimeout);
+    }, []);
+
+    useEffect(() => {
+        let currentChar = 0;
+        let animationInterval;
+
+        const animateTitle = () => {
+            if (currentChar <= titleText.length) {
+                setAnimatedTitle(titleText.slice(0, currentChar));
+                currentChar++;
+            } else {
+                currentChar = 0;
+                setAnimatedTitle("");
+            }
+        };
+
+        if (showTitle) {
+            animationInterval = setInterval(animateTitle, 400); // Délai entre chaque caractère (en millisecondes)
+        }
+
+        return () => {
+            clearInterval(animationInterval);
+        };
+    }, [showTitle]);
+
     return (
         <header className="bg-black-raisin text-champagne p-6">
             <div className="container mx-auto flex justify-between items-center">
-                <h1 className="text-2xl font-semibold underline">My Portfolio</h1>
-                <button
-                    className="lg:hidden"
-                    onClick={toggleNav}
-                >
-                </button>
-                <nav className={`lg:flex ${isNavOpen ? "block" : "hidden"}`}>
+                <div className="flex items-center space-x-2">
+                    <img src={profileImage} alt="Your Profile" className="w-20 h-20 p-2 bg-chamoisee rounded-full" />
+                    {showTitle ? (
+                        <h1 className="text-2xl font-semibold animate-title-in p-2 capitalize font-mono">
+                            {animatedTitle}
+                        </h1>
+                    ) : null}
+                </div>
+                <button className="lg:hidden" onClick={toggleNav}></button>
+                <nav className={`lg:flex ${isNavOpen ? "block" : "hidden"} transition-all duration-300`}>
                     <ul className="lg:flex space-x-6 bg-chamoisee p-4 rounded-md">
-                        <li>
-                            <a href="#about" className="hover:text-black-raisin hover:underline">
-                                About me
+                        <li className="flex items-center hover:text-black-raisin hover:underline">
+                            <User className="w-5 h-5 mr-2" />
+                            <a href="#about" className=" ml-2">
+                                 About me
                             </a>
                         </li>
-                        <li>
-                            <a href="#experience" className=" hover:text-black-raisin hover:underline">
-                                Experience
+                        <li className="flex items-center hover:text-black-raisin hover:underline">
+                            <Experience className="w-5 h-5" />
+                            <a href="#experience" className="ml-2">
+                                 Experience
                             </a>
                         </li>
-                        <li>
-                            <a href="#education" className=" hover:text-black-raisin hover:underline">
-                                Education
+                        <li className="flex items-center hover:text-black-raisin hover:underline">
+                            <School className="w-5 h-5 mr-2" />
+                            <a href="#education" className="  ml-2">
+                                 Education
                             </a>
                         </li>
-                        <li>
-                            <a href="#skills" className=" hover:text-black-raisin hover:underline">
-                                Skills
+                        <li className="flex items-center hover:text-black-raisin hover:underline">
+                            <Skills className="w-5 h-5 mr-2" />
+                            <a href="#skills" className="  ml-2">
+                                 Skills
                             </a>
                         </li>
-                        <li>
-                            <a href="#contact" className=" hover:text-black-raisin hover:underline">
-                                Contact
+                        <li className="flex items-center hover:text-black-raisin hover:underline">
+                            <Contact className="w-5 h-5 mr-2" />
+                            <a href="#contact" className="  ml-2">
+                                 Contact
                             </a>
                         </li>
                     </ul>
